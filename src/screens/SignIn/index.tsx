cff0f1f69f1e0b2bfba089b7e86b771e56838f42
Leo Luna Baima <leobaima01@gmail.com>
2021-07-22 19:12:48 -0300

    Function de navegação | Navigation function

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -11,8 +11,15 @@ import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
 
 import { ButtonIcon } from '../../components/ButtonIcon';
+import { useNavigation } from '@react-navigation/native';
 
-export default function SignIn() {
+export function SignIn() {
+    const navigation = useNavigation();
+
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
     return (
         <View style={styles.container}>
 
@@ -29,7 +36,7 @@ export default function SignIn() {
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Conecte-se {'\n'}
+                    Conecte-se{'\n'}
                     e organize suas{'\n'}
                     jogatinas
                 </Text>
@@ -40,8 +47,8 @@ export default function SignIn() {
                 </Text>
 
                 <ButtonIcon
-                    title="Entrar com discord"
-                    activeOpacity={0.7}
+                    title='Entrar com discord'
+                    onPress={handleSignIn}
                 />
 
             </View>
