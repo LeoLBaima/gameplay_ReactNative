85a1e7f66b10622490e7dd8c00dcaa1d6fb9c250
Leo Luna Baima <leobaima01@gmail.com>
2021-07-22 19:05:17 -0300

    Quase finalizado | Almost finished

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { View, Text } from 'react-native'
 
-import { View } from 'react-native'
-
+import { Avatar } from '../Avatar'
 import { styles } from './styles'
 
 
 export function Profile() {
     return (
-        <View>
-
+        <View style={styles.container}>
+            <Avatar
+                urlImage="https://github.com/LeoLBaima.png"
+            />
+            <View>
+                <View style={styles.user}>
+                    <Text style={styles.greeting}>
+                        Olá,
+                    </Text>
+                    <Text style={styles.username}>
+                        Léo
+                    </Text>
+                </View>
+                <Text style={styles.message}>
+                    Hoje é dia de vitória
+                </Text>
+            </View>
         </View>
     )
 }
