42c1bef8c606cf931ba0681d2383a980a3159323
Leo Luna Baima <leobaima01@gmail.com>
2021-08-12 18:27:03 -0300

    Pequenos ajustes | Little adjustments

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -11,6 +11,7 @@ import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
 
 import { ButtonIcon } from '../../components/ButtonIcon';
+import { Background } from '../../components/Background';
 import { useNavigation } from '@react-navigation/native';
 
 export function SignIn() {
@@ -21,37 +22,38 @@ export function SignIn() {
         navigation.navigate('Home');
     }
     return (
-        <View style={styles.container}>
-
-            <StatusBar
-                barStyle='light-content'
-                backgroundColor='transparent'
-                translucent
-            />
-
-            <Image
-                source={IllustrationImg}
-                style={styles.image}
-                resizeMode='stretch'
-            />
-            <View style={styles.content}>
-                <Text style={styles.title}>
-                    Conecte-se{'\n'}
-                    e organize suas{'\n'}
-                    jogatinas
-                </Text>
-
-                <Text style={styles.subtitle}>
-                    Crie grupos para jogar seus games{'\n'}
-                    favoritos com seus amigos
-                </Text>
-
-                <ButtonIcon
-                    title='Entrar com discord'
-                    onPress={handleSignIn}
+        <Background>
+            <View style={styles.container}>
+                <StatusBar
+                    barStyle='light-content'
+                    backgroundColor='transparent'
+                    translucent
                 />
 
+                <Image
+                    source={IllustrationImg}
+                    style={styles.image}
+                    resizeMode='stretch'
+                />
+                <View style={styles.content}>
+                    <Text style={styles.title}>
+                        Conecte-se{'\n'}
+                        e organize suas{'\n'}
+                        jogatinas
+                    </Text>
+
+                    <Text style={styles.subtitle}>
+                        Crie grupos para jogar seus games{'\n'}
+                        favoritos com seus amigos
+                    </Text>
+
+                    <ButtonIcon
+                        title='Entrar com discord'
+                        onPress={handleSignIn}
+                    />
+                </View>
             </View>
-        </View>
+        </Background>
+
     );
 }
