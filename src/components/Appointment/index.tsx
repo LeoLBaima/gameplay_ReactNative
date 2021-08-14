b3d16e1c24e32f987b01184d78547b6525f3f8f0
Leo Luna Baima <leobaima01@gmail.com>
2021-08-14 17:02:08 -0300

    Ajustes no componente | Adjustments on component

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { View, Text } from 'react-native'
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
-import { categories } from '../../utils/categories'
-import { GuildIcon } from '../GuildIcon'
+import { View, Text } from 'react-native'
 
+import { theme } from '../../global/styles/theme'
 import { styles } from './styles'
 import PlayerSvg from '../../assets/player.svg'
 import CalendarSvg from '../../assets/calendar.svg'
-import { theme } from '../../global/styles/theme'
 
-export type GuildProps = {
-    id: string,
-    name: string,
-    icon: null,
-    owner: boolean
-}
+import { GuildIcon } from '../GuildIcon'
+import { GuildProps } from '../Guild'
+import { categories } from '../../utils/categories'
+import { LinearGradient } from 'expo-linear-gradient'
+
 
 export type AppointmentProps = {
     id: string;
@@ -27,15 +24,21 @@ export type AppointmentProps = {
 type Props = RectButtonProps & {
     data: AppointmentProps
 }
+
 export function Appointment({ data, ...rest }: Props) {
     const [category] = categories.filter(item => item.id === data.category);
     const { owner } = data.guild;
-    const { primary, on } = theme.colors
+    const { primary, on, secondary50, secondary70 } = theme.colors
 
     return (
         <RectButton {...rest}>
             <View style={styles.container}>
-                <GuildIcon />
+                <LinearGradient
+                    style={styles.guildIconContainer}
+                    colors={[secondary50, secondary70]}
+                >
+                    <GuildIcon />
+                </LinearGradient>
 
                 <View style={styles.content}>
                     <View style={styles.header}>
