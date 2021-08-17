3b1cbc73fecb90f16d90be32f8a818f8c89c5fdf
Leo Luna Baima <leobaima01@gmail.com>
2021-08-16 23:52:39 -0300

    Ajustes na log box | log box adjustments

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-
-import { useFonts } from 'expo-font';
+import { StatusBar, LogBox } from 'react-native';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.'])
 
 import { AuthProvider } from './src/hooks/auth';
 
 import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 
-import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
